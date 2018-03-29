a8a1bbcd5f8b0aa19672893268124d039ece9f2a
o10n-x <info@optimization.team>
2018-03-29 17:20:27 +0200

    core update

--- cf-page-cache.php
+++ cf-page-cache.php
@@ -12,7 +12,7 @@ namespace O10n;
  * @wordpress-plugin
  * Plugin Name:       CloudFront Page Cache CDN
  * Description:       Low cost and high performance page cache based on Amazon's CloudFront CDN. CloudFront provides international fast website speed and dedicated geographic IP's.
- * Version:           1.0.29
+ * Version:           1.0.30
  * Author:            Optimization.Team
  * Author URI:        https://optimization.team/
  * Text Domain:       o10n
@@ -29,7 +29,7 @@ if (defined('WP_INSTALLING') && WP_INSTALLING) {
 }
 
 // settings
-$module_version = '1.0.29';
+$module_version = '1.0.30';
 $minimum_core_version = '0.0.16';
 $plugin_path = dirname(__FILE__);
 
