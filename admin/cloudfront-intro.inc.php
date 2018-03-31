e4e1e9c3e0c56a6d92a3c473f73458f6f17dc979
o10n-x <info@optimization.team>
2018-03-31 18:37:20 +0200

    core update

--- admin/cloudfront-intro.inc.php
+++ admin/cloudfront-intro.inc.php
@@ -31,7 +31,13 @@ $module_version = $view->module->version();
 			<p class="about-text" style="min-height:inherit;">Getting started? Read the <a href="https://github.com/o10n-x/wordpress-cloudfront-page-cache/tree/master/docs" target="_blank">setup guide</a> on Github. For professional installation support you can ask your question at the <a href="https://forums.aws.amazon.com/forum.jspa?forumID=46" target="_blank">AWS CloudFront Support Forum</a>.</p>
 
 			<p class="about-text" style="min-height:inherit;">If you are happy with the plugin, please consider to <a href="https://wordpress.org/support/plugin/cf-page-cache/reviews/#new-post" target="_blank" rel="noopener">write a review</a> and <span class="star" style="display:inline-block;vertical-align:middle;"><a class="github-button" data-manual="1" data-size="large" href="https://github.com/o10n-x/wordpress-cloudfront-page-cache" data-icon="octicon-star" data-show-count="true" aria-label="Star o10n-x/wordpress-cloudfront-page-cache on GitHub">Star</a></span> on Github.</p>
-			</div>
+			
+			
+			<hr />
+
+			<h3>WordPress WPO Collection</h3>
+			<p class="about-text">This plugin is part of a <a href="<?php print esc_url(add_query_arg(array('s' => 'o10n', 'tab' => 'search', 'type' => 'author'), admin_url('plugin-install.php'))); ?>">collection of optimization plugins</a> that can be used stand alone or as a selection of plugins with single plugin performance. The goal of the optimization plugins is to achieve the absolute best website performance possible.</p>
+			<p class="about-text">If you are interested in CSS, Javascript or Service Worker optimization, check out our <a href="<?php print esc_url(add_query_arg(array('s' => 'o10n', 'tab' => 'search', 'type' => 'author'), admin_url('plugin-install.php'))); ?>">other plugins</a> that are designed to cooperate with this plugin.</p>
 
 		</div>
 	</div>
